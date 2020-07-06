d52a4d9f9586bd187977d9d059820a283af5621b
Dhanabal210290 <67889702+Dhanabal210290@users.noreply.github.com>
2020-07-06 15:25:34 +0530

    Add files via upload

---
+++ mqtt_ble.ino
@@ -0,0 +1,199 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+#define WIFISSID "Dhana_hot" // Put your WifiSSID here
+#define PASSWORD "Dhanabal21" // Put your wifi password here
+#define TOKEN "BBFF-kpecooK2kMciWztjGt3IqMhe6xqe3b" // Put your Ubidots' TOKEN
+#define MQTT_CLIENT_NAME "hidhana58945"
+// MQTT client Name, please enter your own 8-12 alphanumeric character ASCII string; 
+                                           //it should be a random and unique ascii string and different from all other devices
+
+/****************************************
+ * Define Constants
+ ****************************************/
+#define VARIABLE_LABEL "sensor" // Assing the variable label
+#define DEVICE_LABEL "esp32" // Assig the device label
+
+#define SENSOR 12 // Set the GPIO12 as SENSOR
+
+char mqttBroker[]  = "industrial.api.ubidots.com";
+char payload[100];
+char topic[150];
+// Space to store values to send
+char str_sensor[10];
+
+std::string value = "0";
+/****************************************
+ * Auxiliar Functions
+ ****************************************/
+WiFiClient ubidots;
+PubSubClient client(ubidots);
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+      BLEDevice::startAdvertising();
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic){
+//    std::string 
+    value = pCharacteristic->getValue();
+
+    if(value.length() > 0){
+      Serial.print("Value : ");
+      Serial.println(value.c_str());
+    }
+  }
+
+};
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  char p[length + 1];
+  memcpy(p, payload, length);
+  p[length] = NULL;
+  String message(p);
+  Serial.write(payload, length);
+  Serial.println(topic);
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.println("Attempting MQTT connection...");
+    
+    // Attemp to connect
+    if (client.connect(MQTT_CLIENT_NAME, TOKEN, "")) {
+      Serial.println("Connected");
+    } else {
+      Serial.print("Failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 2 seconds");
+      // Wait 2 seconds before retrying
+      delay(2000);
+    }
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  bleTask();
+
+    WiFi.begin(WIFISSID, PASSWORD);
+  // Assign the pin as INPUT 
+  pinMode(SENSOR, INPUT);
+
+  Serial.println();
+  Serial.print("Wait for WiFi...");
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi Connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  client.setServer(mqttBroker, 1883);
+  client.setCallback(callback);
+
+}
+
+void bleTask(){
+  // Create the BLE Device
+  BLEDevice::init("ESP32 THAT PROJECT");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+
+  pCharacteristic->setCallbacks(new MyCallbacks());
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
+}
+
+String getValue(String data, char separator, int index){
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length()-1;
+
+  for(int i=0; i<=maxIndex && found <=index; i++){
+    if(data.charAt(i)==separator || i==maxIndex){
+      found++;
+      strIndex[0] = strIndex[1]+1;
+      strIndex[1] = (i==maxIndex) ? i+1 : i;
+    }
+  }
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  sprintf(topic, "%s%s", "/v1.6/devices/", DEVICE_LABEL);
+  sprintf(payload, "%s", ""); // Cleans the payload
+  sprintf(payload, "{\"%s\":", VARIABLE_LABEL); // Adds the variable label
+  
+  float sensor = analogRead(SENSOR); 
+  
+  /* 4 is mininum width, 2 is precision; float value is copied onto str_sensor*/
+  dtostrf(sensor, 4, 2, str_sensor);
+  
+//  sprintf(payload, "%s {\"value\": %s}}", payload, str_sensor); // Adds the value
+  sprintf(payload, "%s {\"value\": %s}}", payload, value.c_str()); // Adds the value
+
+  Serial.print("Value : ");
+  Serial.println(value.c_str());
+      
+  Serial.println("Publishing data to Ubidots Cloud");
+  client.publish(topic, payload);
+  client.loop();
+  delay(1000);
+}
---
+++ readme.txt
@@ -0,0 +1,15 @@
+Here i am using ubidots mqtt server for testing this application and use the nRF mobile app to send the data to cloud
+
+SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+ubidots host name and user credentials
+https://industrial.ubidots.com/
+user name: dhanabal
+password: ubidots_123
+
+email-id:dhanabal210290@gmail.com
+
+mqtt publish service:
+Topic/Device: esp32
+Payload/Variable: Sensor
